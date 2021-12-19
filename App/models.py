dc88f30d51ad8460ae3ca7a4205c798da8af65b8
Grecia Ch. Rico <greciarico08@gmail.com>
2021-12-18 20:36:36 -0500

    Agregué modelos, vistas, readme, template con el inicio de la web

--- App/models.py
+++ App/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    nombre_usuario = models.CharField(max_length = 8)
+    email_usuario = models.EmailField()
+    
+class Datos_Personales(models.Model):
+    nombre = models.CharField(max_length = 10)
+    apellido = models.CharField(max_length = 20)
+    dni = models.CharField(max_length = 8)
+    edad = models.IntegerField()
+    nacionalidad = models.CharField(max_length = 20)
+
+class Lugar_de_destino(models.Model):
+    pais_latinoamericano = models.CharField(max_length = 30)
+    ciudad = models.CharField(max_length = 30)
+    fecha_de_viaje = models.DateTimeField()
---
+++ README.md
@@ -0,0 +1,10 @@
+PROYECTO WEB
+Proyecto web que va dirigido para las personas que aman viajar. En esta web 
+pueden encontrar los mejores destinos para hacer turismo en Latinoamerica.
+Sigue estos pasos para probar mi proyecto:
+- Iniciar pipenv 
+- Crear un superuser 
+- Migrar django para generar la base de datos
+- Crear modelos
+- Hacer un makemigrations 
+- Crear las vistas, urls y templates.
